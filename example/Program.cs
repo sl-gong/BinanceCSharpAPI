62e997433b713f0051ed3e5ec60d450cb2593f66
sl-gong <50310392@qq.com>
2017-10-27 17:37:39 +0800

    Add files via upload

---
+++ example/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // api_key and secret_key can get from your binance account
+            BinanceExchangeCSharp.BinanceCSharpAPI.setKey("your api key",
+                "your secret key");
+
+            string json_result = "";
+            BinanceExchangeCSharp.BinanceCSharpAPI.get_ping(ref json_result);
+
+            BinanceExchangeCSharp.BinanceCSharpAPI.get_serverTime();
+
+            BinanceExchangeCSharp.BinanceCSharpAPI.get_depth("BNBBTC", ref json_result);
+            JObject depth = JObject.Parse(json_result);
+            JArray bids = depth["bids"] as JArray;
+            JToken bid0Price = bids[0][0];
+            JToken bid0quantity = bids[0][1];
+
+            JArray asks = depth["asks"] as JArray;
+
+            BinanceExchangeCSharp.BinanceCSharpAPI.get_24hr("BNBBTC", ref json_result);
+
+            BinanceExchangeCSharp.BinanceCSharpAPI.get_aggTrades("BNBBTC", ref json_result);
+
+            BinanceExchangeCSharp.BinanceCSharpAPI.get_allBookTickers(ref json_result);
+
+            BinanceExchangeCSharp.BinanceCSharpAPI.get_allOrders("BNBBTC", ref json_result);
+
+            BinanceExchangeCSharp.BinanceCSharpAPI.get_allPrices(ref json_result);
+            JArray allPrices = JArray.Parse(json_result);
+            for (int i = 0; i < allPrices.Count; i++)
+            {
+                JToken symbol = allPrices[i]["symbol"];
+                JToken price = allPrices[i]["price"];
+            }
+
+            BinanceExchangeCSharp.BinanceCSharpAPI.get_depth("BNBBTC", ref json_result);
+
+            BinanceExchangeCSharp.BinanceCSharpAPI.get_klines("BNBBTC", "1d", ref json_result);
+
+            BinanceExchangeCSharp.BinanceCSharpAPI.get_myTrades("BNBBTC", ref json_result);
+
+            BinanceExchangeCSharp.BinanceCSharpAPI.get_openOrders("BNBBTC", ref json_result);
+
+            BinanceExchangeCSharp.BinanceCSharpAPI.get_order("BNBBTC", ref json_result, 0, "ZFb87ptNydZNBwVoTBeUdR");
+
+            BinanceExchangeCSharp.BinanceCSharpAPI.get_account(ref json_result);
+
+            BinanceExchangeCSharp.BinanceCSharpAPI.post_order("BNBBTC", "BUY", "LIMIT", "GTC", 100, 0.00001, ref json_result);
+
+            int testi = 0;
+        }
+    }
+}
